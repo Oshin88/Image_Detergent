49389e0d0dacb7bd94c9446a5d124c8b9b7c7f3f
Alec Shin <olegshin19@gmail.com>
2016-03-08 16:25:11 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,17 +4,9 @@ detergent = Detergent()
 pics = detergent.ListFiles('./pic/')
 prefix_dir = "./pic/"
 
-'''
+#iterates through all the provided images within the pic folder
 for pic in pics:
 	img_path = prefix_dir + pic
 	print(img_path)
 	(blur, extent) = detergent.Get_Edge_Intensity(img_path, 35)
 	detergent.is_Blurry(0.05, blur, extent) 
-'''
-
-img_path = './pic/blurry4.jpg'
-(blur, extent) = detergent.Get_Edge_Intensity(img_path, 35)
-detergent.is_Blurry(0.05, blur, extent) 
-
-
-
